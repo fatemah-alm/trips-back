8240bee41cb10aa1f9507be7f704821b595eec63
fatemah-alm <fatemah.alm@hotmail.com>
2022-03-08 20:58:05 +0400

    relation profile

--- api/user/controllers.js
+++ api/user/controllers.js
@@ -2,6 +2,7 @@ const User = require("../../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET, JWT_EXPIRATION_MS } = require("../../config./keys");
+const Profile = require("../../models/Profile");
 
 exports.signup = async (req, res, next) => {
   console.log(req.body);
@@ -12,9 +13,13 @@ exports.signup = async (req, res, next) => {
     console.log("exports.signup -> hashedPassword", hashedPassword);
     req.body.password = hashedPassword;
     const newUser = await User.create(req.body);
+    const newProfile = await Profile.create({ owner: newUser._id });
+    const updatedUser = await User.findByIdAndUpdate(newUser._id, {
+      profile: newProfile._id,
+    });
     const payload = {
-      id: newUser._id,
-      username: newUser.username,
+      id: updatedUser._id,
+      username: updatedUser.username,
       exp: Date.now() + JWT_EXPIRATION_MS,
     };
     const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
@@ -34,3 +39,17 @@ exports.signin = (req, res, next) => {
   const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
   res.status(201).json({ token });
 };
+
+// exports.profileCreate = async (req, res, next) => {
+//   try {
+//     if (req.file) {
+//       req.body.image = `/${req.file.path}`;
+//       req.body.image = req.body.image.replace("\\", "/");
+//     }
+//     req.body.owner = req.user._id;
+//     const newProfile = await Profile.create(req.body);
+//     return res.status(201).json(newProfile);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
